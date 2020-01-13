8a3a93d16b5c50c0f5efc8a8fca69eea04af1d80
Krzysztof Szyda <krzysztof.szyda@netguru.pl>
2020-01-13 13:16:49 +0100

    headers added

--- locust_converter/command_line.py
+++ locust_converter/command_line.py
@@ -2,31 +2,38 @@ import argparse
 import json
 import os
 import pathlib
+from json import JSONDecodeError
 from string import Template
-from xml.etree import ElementTree
-from xml.etree.ElementTree import ParseError
+from lxml import etree
+from lxml.etree import XMLSyntaxError
 
 from yapf.yapflib.yapf_api import FormatCode
+
+HEADER_NAME_TAG = 'stringProp[@name="Header.name"]'
+HEADER_VALUE_TAG = 'stringProp[@name="Header.value"]'
 
 
 def is_valid_file(parser_handler, filename):
     if not os.path.exists(filename):
-        parser_handler.error("The file {filename} does not exist!")
+        parser_handler.error(f"The file {filename} does not exist!")
     else:
         return filename
 
 
-def get_xml(filename, test_tag='.//HTTPSamplerProxy'):
+def get_xml_root(filename):
     try:
-        return ElementTree.parse(filename).findall(test_tag)
-    except ParseError:
+        with open(filename) as file:
+            return etree.parse(file)
+    except XMLSyntaxError:
         raise Exception(f'File {filename} is not correct XML file')
 
 
+def get_element_tags(element, test_tag='.//HTTPSamplerProxy'):
+    return element.findall(test_tag)
+
+
 def get_attribute_value(element, attribute_name=''):
-    for node in element.findall('stringProp'):
-        if node.attrib['name'] == attribute_name:
-            return node.text
+    return element.find(f'stringProp[@name="{attribute_name}"]').text
 
 
 def get_params(element):
@@ -34,27 +41,61 @@ def get_params(element):
     if node is not None:
         if not node.text:
             return
-        return str(json.loads(node.text))
+        try:
+            return str(json.loads(node.text))
+        except JSONDecodeError:
+            return
 
 
-def get_urls_from_xml(name):
+def get_header(element, path='HeaderManager/collectionProp/elementProp'):
+    node = element.findall(path)
+    if not node:
+        return
+    headers = {}
+    for element_prop in node:
+        headers[element_prop.find(HEADER_NAME_TAG).text] = element_prop.find(HEADER_VALUE_TAG).text
+    return headers
+
+
+def get_base_header(root):
+    """
+    Base header used in all test cases is defined in the end of the jmeter XML file
+    """
+    element = root.find('.//hashTree/hashTree/HeaderManager')
+    return get_header(element, path='collectionProp/elementProp')
+
+
+def get_test_case_header(element):
+    """
+    Header is the next element in the XML tree.
+    In order to get the header details, we need to check the next element after HTTPSamplerProxy.
+    If  "HeaderManager" does not exist in the next element, this test case has no specific header.
+    """
+    return get_header(element.getnext())
+
+
+def get_urls_from_xml(filename):
     urls = []
-    for element in get_xml(filename=name):
+    root = get_xml_root(filename=filename)
+    base_header = get_base_header(root)
+    for element in get_element_tags(root):
         enabled = element.attrib.get('enabled')
         if enabled:
             endpoint = get_attribute_value(element, 'HTTPSampler.path')
             method = get_attribute_value(element, 'HTTPSampler.method')
             params = get_params(element)
-            url = f"{{'method': '{method}', 'url': '{endpoint}', 'params': {params}}},"
+            headers = get_test_case_header(element) or base_header
+            url = f"{{'method': '{method}', 'url': '{endpoint}', 'params': {params}, 'headers': {headers}}},"
             urls.append(url)
     return ''.join(urls)
 
 
 def generate_file(base_file, filename='locustfile.py'):
-    urls = get_urls_from_xml(name=base_file)
+    urls = get_urls_from_xml(filename=base_file)
+    additional_variables = ''
     with open(f'{pathlib.Path(__file__).parent}/locustfile.template') as template:
         template_file = Template(template.read())
-        result_data = template_file.substitute(urls=urls)
+        result_data = template_file.substitute(urls=urls, additional_variables=additional_variables)
 
     with open(filename, 'w') as file:
         file.write(FormatCode(result_data)[0])
